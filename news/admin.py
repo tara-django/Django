2efdd93639eb87d81ce4111dfe8519ec0b8ddd68
tara-django <tara.dhamala@nepasoft.com>
2020-01-19 07:14:07 +0545

    News-Add final

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from news.models import News, Category
 # Register your models here.
+
+
+@admin.register(News)
+class NewsAdmin(admin. ModelAdmin):
+    list_display = ["title", "author", "created_at"]
+    prepopulated_fields = {"slug" : ("title",)}
+
+
+admin.site.register(Category)
+
