ae9659fdd66549788ccb5f99b16a8b2e383268be
tara-django <tara.dhamala@nepasoft.com>
2020-01-31 07:18:11 +0545

    accounts code

--- news/context_processors.py
+++ news/context_processors.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from news.models import Category
 
 
-
 def categories(request):
-    category_list = Category.objects.all()
-    return {"categories" : category_list}
+   category_list = Category.objects.all()
+   return {"categories": category_list}
